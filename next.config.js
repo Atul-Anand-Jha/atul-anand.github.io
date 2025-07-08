05724a2eebda62cf3003682082216fe95eb0b3c8
Atul Anand <atul.anand@cars24.com>
2025-07-08 15:44:59 +0530

    Add basePath configuration for GitHub Pages project repository
    - Added basePath and assetPrefix for /atul-anand.github.io
    - This fixes the URL routing for the current repository structure
    - Site will now work correctly at atul-anand-jha.github.io/atul-anand.github.io/

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/atul-anand.github.io',
+  assetPrefix: '/atul-anand.github.io',
   images: {
     unoptimized: true
   }
